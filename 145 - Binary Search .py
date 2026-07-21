553c318344010ff67aa504d7b214c57bea93b0ea
Saurab Gyawali <153341258+soorabcode@users.noreply.github.com>
2026-07-21 16:25:14 +0545

    Add files via upload

---
+++ 145 - Binary Search .py
@@ -0,0 +1,19 @@
+# Day 145 
+# Binary Search 
+# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
