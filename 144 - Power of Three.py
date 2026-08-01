004bddc278a3408fc0250b5073dee556dd58fdd2
Saurab Gyawali <153341258+soorabcode@users.noreply.github.com>
2026-08-01 13:01:50 +0545

    Add files via upload

--- 144 - Power of Three.py
+++ 144 - Power of Three.py
@@ -1,3 +1,12 @@
 # Day 144 
 # Power of Three 
-# 
+# Given an integer n, return true if it is a power of three. Otherwise, return false.
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        while n % 3 == 0:
+            n //= 3
+
+        return n == 1
