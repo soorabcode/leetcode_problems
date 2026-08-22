672acd19caa8f4e90c369192c8a837e24ea05c09
Saurab Gyawali <153341258+soorabcode@users.noreply.github.com>
2026-08-22 14:52:46 +0545

    Add files via upload

--- 149 - Partition Equal Subset Sum .py
+++ 149 - Partition Equal Subset Sum .py
@@ -1,4 +1,22 @@
 # Day 149 
 # Partition Equal Subset Sum
 # https://leetcode.com/problems/partition-equal-subset-sum/
-# 
+# Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+
+        if total % 2 != 0:
+            return False
+
+        target = total // 2
+
+        dp = [False] * (target + 1)
+        dp[0] = True
+
+        for num in nums:
+            for j in range(target, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
+
+        return dp[target]
